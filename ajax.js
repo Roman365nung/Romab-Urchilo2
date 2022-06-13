725976c186880f8c298b1cbc6b08640e713ce571
Roman365nung <95237920+Roman365nung@users.noreply.github.com>
2022-06-13 16:08:26 +0300

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,15 @@
+ <td>`+data[i].id+`</td>
+            <td>`+data[i].NameInput+`</td>
+            <td>`+data[i].LocationInput+`</td>
+            <td>`+data[i].DateInput+`</td>
+            <td>`+data[i].StatutoryfundInput+`</td>
+            <td>`+data[i].HeadInput+`</td>
+            <td>`+data[i].ActivityInput+`</td>
+            <td>`+data[i].NomberofjobsInput+`</td>
+            <td>`+data[i].ComissioningYear+`</td>
+            <td><a class="delete" href="#" data-id="`+data[i].id+`">Delete</a>
+          </tr>
+          `);
+      }
+    });
+  });
